6d57aa0d9f0bbe21b72ccbb99ad502fce2520831
NoRIS95 <ryzhikov95@gmail.com>
2024-02-05 14:58:05 +0300

    Table queries are optimized

--- main.py
+++ main.py
@@ -10,65 +10,52 @@ load_dotenv()
 n_top=10
 SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 SHEET_NAME = os.getenv('SHEET_NAME')
-SLEEP_TIME_LOOP = 30
-SLEEP_TIME_REQUEST = 1
+SLEEP_TIME_LOOP = 120
 EXCHANGES = ["Биржа", "Сова", 'NetEx24', 'Шахта', 'Ферма']
 CRIPT_LIST = ['BTC', 'ETH', 'TON', 'USDT', 'XMR', 'TRX']
 MONETARY_CUR_LIST = ['USDT', 'RUB']
 MONETARY_CUR_LIST_TOP = ['RUB', 'USDT']
-ALPHABET = list((''.join([chr(i) for i in range(99, 114)])).upper())
-ALPHABET_FOR_TOP = list((''.join([chr(i) for i in range(99, 112)])).upper())
-STRING_NUMS_LIST = [i for i in range(5,11)]
-STRING_NUMS_LIST_TOP = [i for i in range(26,26 + n_top)]
 row_ind = 0
 CELL_CALLBACKS = {}
 
 
 for cript in CRIPT_LIST:
-    row = STRING_NUMS_LIST[row_ind] 
     col_ind = 0
     for exchange in EXCHANGES:
         for money in MONETARY_CUR_LIST:
-            letter = ALPHABET[col_ind]
-            cell = letter + str(row)
             if exchange == "Биржа":
-                CELL_CALLBACKS[cell] = (get_actual_price, (cript, money))
+                CELL_CALLBACKS[(row_ind, col_ind)] = (get_actual_price, (cript, money))
             else:
-                CELL_CALLBACKS[cell] = (get_best_rate, (exchange, cript, money))
+                CELL_CALLBACKS[(row_ind, col_ind)] = (get_best_rate, (exchange, cript, money))
             col_ind += 1
         col_ind += 1
     row_ind += 1
 
 
 if __name__ == '__main__':
-    sa = SpreadsheetApp('secret.json')
-    spreadsheet = sa.open_by_id(SPREADSHEET_ID)
-    sheet = spreadsheet.get_sheet_by_name(SHEET_NAME)
+    sheet = SpreadsheetApp('secret.json').open_by_id(spreadsheet_id=SPREADSHEET_ID).get_sheet_by_name(SHEET_NAME)
     while True:
+        data_range = sheet.get_range_from_a1('C5:P35')
+        backgrounds = data_range.get_backgrounds()
+        values = data_range.get_values()
         top_exchanges = get_top()
         
-        row_ind_top = 0
+        row_ind_top = 26 - 5  # топ начинается с 26 строчки, а значения нашей таблицы с 5
         for exch in top_exchanges:
-            row_top = STRING_NUMS_LIST_TOP[row_ind_top]
-            let = ALPHABET_FOR_TOP[0]
-            cl = let + str(row_top)
-            CELL_CALLBACKS[cl] = (lambda x: x, [exch])
-            col_ind_top = 1
+            col_ind_top = 0
+            CELL_CALLBACKS[(row_ind_top, col_ind_top)] = (lambda x: x, [exch])
             for cr in CRIPT_LIST:
                 for mon in MONETARY_CUR_LIST_TOP:
-                    let = ALPHABET_FOR_TOP[col_ind_top]
-                    cl = let + str(row_top)
-                    CELL_CALLBACKS[cl] = (get_best_rate, (exch, cr, mon))
                     col_ind_top += 1
+                    CELL_CALLBACKS[(row_ind_top, col_ind_top)] = (get_best_rate, (exch, cr, mon))
             row_ind_top += 1
         
-        for cell_id, (fn, args) in CELL_CALLBACKS.items():
+        for (row_ind, col_ind), (fn, args) in CELL_CALLBACKS.items():
             value = fn(*args)
             if value is None:
                 value = '-'
-            cell = sheet.get_range_from_a1(cell_id)
-            cell.set_value(value)
-            time.sleep(SLEEP_TIME_REQUEST)
-
-
+            values[row_ind][col_ind] = value
+        data_range.set_values(values)
+        data_range.set_backgrounds(backgrounds)
+        # data_range.commit()
         time.sleep(SLEEP_TIME_LOOP)
